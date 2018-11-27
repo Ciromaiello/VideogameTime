8cdcd6a6a16de85314c24e797c7820fa4a17d102
peppecacace <giuseppe.cacace10@gmail.com>
2018-11-27 13:00:54 +0100

    primo prototipo di database su firebase creato da codice, creata classe Giochi sempre per l'inserimento di nuovi giochi, la classe Prova mi serve per richiamare parti di codice utili

--- app/src/main/java/com/example/cirom/videogametime/MainActivity.java
+++ app/src/main/java/com/example/cirom/videogametime/MainActivity.java
@@ -5,13 +5,36 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.lang.ref.Reference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference databaseGiochi;
+    DatabaseReference ref;
+    String[] id = new String[10];
+    int cont=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         impostaPager();
+
+        databaseGiochi = FirebaseDatabase.getInstance().getReference("giochi");
+
+        //esempi di inserimenti fatti da noi di giochi
+/*
+        inserisciGioco("Fifa 19","Sportivo","PlayStation 4,XBOX ONE");
+        inserisciGioco("Football Manager 2019","Gestionale,Sportivo","PS VITA");
+        inserisciGioco("Pokémon Smeraldo ","GDR","Nintendo DS,Nintendo 3DS");
+        inserisciGioco("ARMS","Picchiaduro","Nintendo SWITCH");
+*/
     }
 
     private void impostaPager() {
@@ -30,4 +53,33 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout.setupWithViewPager(viewPager, true);
     }
+
+    public void inserisciGioco(String nome,String genere,String piattaforma) {
+
+
+        //metodo per inserire un gioco nel database da codice
+
+
+        ref = databaseGiochi.push().getRef();
+        id[cont] =ref.getKey();
+        ref.setValue(new Giochi(nome,genere,piattaforma,id[cont]));
+        cont++;
+
+    }
+
+
+    //metodo alternativo per inserimento di giochi ma sovrascrive il database ogni volta
+        /*Map<String, Giochi> giochi = new HashMap<>();
+        String[] id = new String[10];
+        for (int i=0;i<10;i++)
+        {
+            id[i]=databaseGiochi.push().getKey();
+        }
+        giochi.put(id[0],new Giochi("Fifa 19", "Sportivo", "PS4,XBOX ONE",id[0]));
+        giochi.put(id[1],new Giochi("ARMS","picchiaduro","NINTENDO SWITCH",id[1]));
+        giochi.put(id[2],new Giochi("Football Manager 2019","Simulazione","PC",id[2]));
+        databaseGiochi.setValue(giochi);
+    */
+
+
 }
