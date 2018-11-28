ecfd89ac7aa9ed1a00cc99b7ccba20ed78618c9a
Ciro Maiello <ciro.mai@hotmail.it>
2018-11-28 01:36:36 +0100

    Cambiato aspetto activity di accesso

--- app/src/main/java/com/example/cirom/videogametime/AccessoUtenti.java
+++ app/src/main/java/com/example/cirom/videogametime/AccessoUtenti.java
@@ -1,6 +1,5 @@
 package com.example.cirom.videogametime;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +23,4 @@ public class AccessoUtenti extends AppCompatActivity {
             }
         });
     }
-
-
 }
