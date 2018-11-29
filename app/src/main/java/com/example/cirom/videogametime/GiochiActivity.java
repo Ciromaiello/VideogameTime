a3415c8522f39864ef5164ed30ae65eca79770f8
AntoNoga <38253602+AntoNoga@users.noreply.github.com>
2018-11-29 03:00:59 +0100

    cambiati nomi funzionante

--- app/src/main/java/com/example/cirom/videogametime/GiochiActivity.java
+++ app/src/main/java/com/example/cirom/videogametime/GiochiActivity.java
@@ -1,26 +1,27 @@
-package com.example.cirom.videogametime;
+//package com.example.cirom.videogametime;
 
-import android.content.Intent;
+//import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class GiochiActivity extends AppCompatActivity {
+//public class GiochiActivity extends AppCompatActivity {
 
-    private Button btnScelta;
+  //  private Button btnScelta;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.utenti_accesso);
-        btnScelta = findViewById(R.id.ospite_btn);
-        btnScelta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(GiochiActivity.this,ProfiloActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-}
+    //@Override
+   //7
+// protected void onCreate(Bundle savedInstanceState) {
+   //     super.onCreate(savedInstanceState);
+     //   setContentView(R.layout.activity_giochi);
+       // btnScelta = findViewById(R.id.ospite_btn);
+        //btnScelta.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View v) {
+              //  Intent intent = new Intent(GiochiActivity.this,ProfiloActivity.class);
+                //startActivity(intent);
+            //}
+        //});
+    //}
+//}
